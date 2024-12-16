33d4432e9cfb67b783145c850fdcb27f9b775d28
c-zawolo <czawolo03@vt.edu>
2024-12-16 04:11:39 -0500

    Update sketch.js
    I genuinely have no clue why the updates I've been making for hours just. Didn't land here? Either way I just copied my code into here and called it a day

--- sketch.js
+++ sketch.js
@@ -1,66 +1,83 @@
 let font;
 let sqBrush;
-let rdBrush;
+let swBrush = 0;
 let erBrush;
-let bg;
-let currentcolor;
-let brushShape;
-let colorSlider;
+let sizeSlider;
+let brightSlider;
+let satSlider;
+let myBtl1;
+let myBtl2;
 
-function preload(){}
+
+
 function setup() {
-  createCanvas(384, 224);
-  background(220);
-  strokeJoin(MITER);
+  canvas = createCanvas(900, 300);
+  canvas.parent('canvoid')
+  rectMode(CENTER);
+  angleMode(DEGREES);
+  background(0,5,73);
+  colorMode(HSB);
+   
+  sizeSlider = createSlider(1,10,1)
+  sizeSlider.parent('buttoid');
+  sizeSlider.value(10)
 
-  //colorSlider = createSlider()
   let button1 = createButton("Save your Work");
-  button1.parent("buttoid");
+  button1.position("buttoid");
   button1.mousePressed(saveWork)
 
   let button2 = createButton("Delete your Work");
   button2.parent("buttoid");
   button2.mousePressed(dltWork)
 
-  //let sqBrush = new Brush()
-  //let rdBrush = new Brush()
-  //let erBrush = new Brush()
+  colorPicker = createColorPicker('#000000');
+  colorPicker.position(width/1.05,height/1.25);
+  myBtl1 = new Btl(color(242,69,22),35,280);
+  myBtl2 = new Btl(color(242,69,22),85,280);
+
+
 }
 
 function draw() {
-  //sqBrush.display()
-  //rdBrush.display()
-  //erBrush.display()
-}
+  myBtl1.display();
+  myBtl2.display();
 
-//function keyReleased(){
-  //if (key == 'B'||key == 'b')
-//}
+  noStroke();
+  fill(236,92,23);
+  rect(450,0,width,height/4);
+  
+
+  if (mouseIsPressed){
+    noStroke();
+   fill(colorPicker.color());
+    rect(mouseX, mouseY,sizeSlider.value());
+  }
+  }
+
+
 function saveWork(){
-  //image()
   saveCanvas("seeyanextgame.jpg");
 }
 
 function dltWork(){
-  background(220);
+  background(236,100,29);
 }
 
-//function mousePressed() {
-  //if (mouseX < width && mouseX > 0) {
-    //if (mouseY < height && mouseY > 0) {
-      //image(if i can get the class in here, it's that, mouseX, mouseY, 15, 15);
-    //}
-  //}
-//}
-
-
-//class Brush{constructor(c,xpos,ypos,bsize){
-//this.c = c;
-//this.xpos = xpos;
-//this.ypos = ypos;
-//this.bsize = bsize;}
- //display(){
- //noStroke();
- //fill(this.c);
- //}
-//}
+class Btl {
+  constructor(c, xpos, ypos) {
+    this.c = c;
+    this.xpos = xpos;
+    this.ypos = ypos;
+  }
+  display() {
+    noStroke();
+    fill(240, 61, 14);
+    ellipse(this.xpos, this.ypos, 25, 50);
+    fill(this.c);
+    arc(this.xpos, this.ypos - 15, 20, 25, 180, 0);
+    fill(238, 45, 68);
+    rect(this.xpos, this.ypos + 5, 2, 40, 0, 0, 5, 5);
+    rect(this.xpos - 10, this.ypos - 15, 2, 25);
+    rect(this.xpos + 10, this.ypos - 15, 2, 25);
+  }
+}
